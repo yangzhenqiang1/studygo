70474ba379d0700959275fe485de8fba4151ee5b
Your Name <you@example.com>
2020-12-13 17:23:24 +0800

    数组部分

--- arr/main.go
+++ arr/main.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-//arr 数组的三种初始化方式
-
-//重点注意：数组的长度和类型也属于数组的一部分
+//重点注意：
+//1.数组的长度和类型也属于数组的一部分
+//2.数组是值类型
 func main() {
+	/*arr 数组的三种初始化方式 start*/
+
 	//数组的第一种初始化方式
 	var arr1 [30]int
 	arr1 = [30]int{1, 2, 3, 4, 5}
@@ -18,5 +20,40 @@ func main() {
 	//第三种
 	arr3 := [...]int{1: 1, 99: 122}
 	fmt.Println(arr3)
+	/*arr 数组的三种初始化方式 end*/
 
+	/*数组的遍历 start*/
+	//第一种遍历方式  索引取值
+	for i := 0; i < len(arr1); i++ {
+		fmt.Println(arr1[i])
+	}
+	//第二种遍历方式  键值对方式
+	for index, value := range arr2 {
+		fmt.Println(index, value)
+	}
+	/*数组的遍历 end*/
+
+	/*多维数组的定义 start*/
+	a := [3][2]string{
+		{"北京", "上海"},
+		{"广州", "深圳"},
+		{"成都", "重庆"},
+	}
+	fmt.Println("多维数组：", a)         //[[北京 上海] [广州 深圳] [成都 重庆]]
+	fmt.Println("多维数组取值：", a[2][1]) //支持索引取值:重庆
+	/*多维数组的定义 end*/
+
+	/*多维数组的遍历 start*/
+	a1 := [3][2]string{
+		{"北京", "上海"},
+		{"广州", "深圳"},
+		{"成都", "重庆"},
+	}
+	for _, v1 := range a1 {
+		for _, v2 := range v1 {
+			fmt.Printf("%s\t", v2)
+		}
+		fmt.Println()
+	}
+	/*多维数组的遍历 end*/
 }
