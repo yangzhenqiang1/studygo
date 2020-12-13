df364a41213545b628b07039db940394def1d6ff
Your Name <you@example.com>
2020-12-13 16:45:11 +0800

    数组的三种初始化方式

--- arr/main.go
+++ arr/main.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+//arr 数组的三种初始化方式
 func main() {
 	//数组的第一种初始化方式
 	var arr1 [30]int
 	arr1 = [30]int{1, 2, 3, 4, 5}
 	fmt.Println(arr1)
-	
+
+	//第二种
+	arr2 := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 41, 22}
+	fmt.Println(arr2)
+
+	//第三种
+	arr3 := [...]int{1: 1, 99: 122}
+	fmt.Println(arr3)
+
 }
